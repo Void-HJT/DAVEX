860a51cf296ba03415b6b6611aaebc9daf891b48
nhysteric <hyniu23@m.fudan.edu.cn>
2024-07-21 04:28:30 -0400

    feat: 为center添加ssl配置

---
+++ DVE_center/src/main/java/DveCenter/common/My.java
@@ -0,0 +1,248 @@
+package DveCenter.common;
+
+import java.io.InputStream;
+import java.security.Key;
+import java.security.KeyStore;
+import java.security.PrivateKey;
+import java.security.Signature;
+import java.security.cert.Certificate;
+import java.util.Base64;
+import DveAgent.common.Utlis;
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Component;
+
+import DveAgent.entity.Center;
+import nl.altindag.ssl.SSLFactory;
+
+@Component
+public class My {
+    @Value("${version}")
+    private String version;
+
+    @Value("${my.name}")
+    private String name;
+
+    private Center center;
+
+    public Center getCenter() {
+        return center;
+    }
+
+    public void setCenter(Center center) {
+        this.center = center;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Value("${my.id}")
+    private int id;
+
+    @Value("${my.ip}")
+    private String ip;
+
+    @Value("${server.port}")
+    private int port;
+
+    @Value("${ssl.keystore-path}")
+    private String keyStorePath;
+
+    @Value("${ssl.keystore-password}")
+    private String keyStorePassword;
+
+    @Value("${ssl.truststore-path}")
+    private String trustStorePath;
+
+    @Value("${ssl.truststore-password}")
+    private String trustStorePassword;
+
+    @Value("${ssl.key-store-type}")
+    private String keyStoreType;
+
+    private KeyStore keyStore;
+
+    private KeyStore trustStore;
+
+    private PrivateKey privateKey;
+
+    private Certificate certificate;
+
+    private SSLFactory baseSslFactory;
+
+    public SSLFactory getBaseSslFactory() {
+        return baseSslFactory;
+    }
+
+    public void setBaseSslFactory(SSLFactory baseSslFactory) {
+        this.baseSslFactory = baseSslFactory;
+    }
+
+    public Certificate getCertificate() {
+        return certificate;
+    }
+
+    public void setCertificate(Certificate certificate) {
+        this.certificate = certificate;
+    }
+
+    // @Autowired
+    // ResourceLoader resourceLoader;
+
+    @PostConstruct
+    public void init() throws Exception {
+
+        try (InputStream inputStream = new ClassPathResource(keyStorePath).getInputStream()) {
+            keyStore = KeyStore.getInstance(keyStoreType);
+            keyStore.load(inputStream, keyStorePassword.toCharArray());
+            Key key = keyStore.getKey(name, keyStorePassword.toCharArray());
+            if (key instanceof PrivateKey) {
+                privateKey = (PrivateKey) key;
+                certificate = keyStore.getCertificate(name);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to load keys from keystore", e);
+        }
+
+        trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
+        trustStore.load(null, null);
+
+        try (InputStream inputStream = new ClassPathResource(trustStorePath).getInputStream()) {
+            trustStore = KeyStore.getInstance(keyStoreType);
+            trustStore.load(inputStream,
+                    trustStorePassword.toCharArray());
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to load trustStore", e);
+        }
+        baseSslFactory = SSLFactory.builder()
+                .withIdentityMaterial(keyStore, keyStorePassword.toCharArray())
+                .withSwappableIdentityMaterial().withSwappableTrustMaterial()
+                .withTrustMaterial(trustStore)
+                .build();
+        center = new Center();
+        center.setUid(id);
+        center.setName(name);
+        center.setIp(ip);
+        center.setPort(port);
+        center.setCrt(Utlis.certificateToBytes(certificate));
+    }
+
+    /**
+     * 签名数据
+     * 
+     * @param data 待签名数据
+     * @return 签名后的字符串
+     * @throws Exception
+     */
+    public String signData(byte[] data) throws Exception {
+        Signature signature = Signature.getInstance("SHA256withRSA");
+        signature.initSign(privateKey);
+        signature.update(data);
+        byte[] signedData = signature.sign();
+        return Base64.getEncoder().encodeToString(signedData);
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public PrivateKey getPrivateKey() {
+        return privateKey;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public void setPrivateKey(PrivateKey privateKey) {
+        this.privateKey = privateKey;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public KeyStore getKeyStore() {
+        return keyStore;
+    }
+
+    public void setKeyStore(KeyStore keyStore) {
+        this.keyStore = keyStore;
+    }
+
+    public KeyStore getTrustStore() {
+        return trustStore;
+    }
+
+    public void setTrustStore(KeyStore trustStore) {
+        this.trustStore = trustStore;
+    }
+
+    public String getKeyStorePath() {
+        return keyStorePath;
+    }
+
+    public void setKeyStorePath(String keyStorePath) {
+        this.keyStorePath = keyStorePath;
+    }
+
+    public String getKeyStorePassword() {
+        return keyStorePassword;
+    }
+
+    public void setKeyStorePassword(String keyStorePassword) {
+        this.keyStorePassword = keyStorePassword;
+    }
+
+    public String getTrustStorePath() {
+        return trustStorePath;
+    }
+
+    public void setTrustStorePath(String trustStorePath) {
+        this.trustStorePath = trustStorePath;
+    }
+
+    public String getTrustStorePassword() {
+        return trustStorePassword;
+    }
+
+    public void setTrustStorePassword(String trustStorePassword) {
+        this.trustStorePassword = trustStorePassword;
+    }
+
+    public String getKeyStoreType() {
+        return keyStoreType;
+    }
+
+    public void setKeyStoreType(String keyStoreType) {
+        this.keyStoreType = keyStoreType;
+    }
+
+}
--- DVE_center/src/main/java/DveCenter/module/auth/service/CenterWebClientService.java
+++ DVE_center/src/main/java/DveCenter/module/auth/service/CenterWebClientService.java
@@ -1,4 +1,4 @@
-package DveAgent.module.auth.service;
+package DveCenter.module.auth.service;
 
 import java.security.KeyStore;
 import java.security.cert.Certificate;
@@ -13,10 +13,9 @@ import org.springframework.web.reactive.function.client.WebClient;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 
-import DveAgent.common.My;
+import DveCenter.common.My;
 import DveAgent.common.Utlis;
 import DveAgent.entity.Agent;
-import DveAgent.entity.Center;
 import DveAgent.mapper.AgentMapper;
 import DveAgent.mapper.CenterMapper;
 import io.netty.handler.ssl.SslContext;
@@ -24,7 +23,7 @@ import io.netty.handler.ssl.SslContextBuilder;
 import reactor.netty.http.client.HttpClient;
 
 @Service
-public class AgentWebClientService {
+public class CenterWebClientService {
         @Autowired
         private CenterMapper centerMapper;
         @Autowired
@@ -35,25 +34,7 @@ public class AgentWebClientService {
         @Autowired
         My my;
 
-        public WebClient agent2CenterWebClient(int center_id) throws Exception {
-                LambdaQueryWrapper<Center> queryWrapper = Wrappers.<Center>lambdaQuery().eq(Center::getUid, center_id);
-                Center center = centerMapper.selectOne(queryWrapper);
-                Certificate certificate = Utlis.bytesToCertificate(center.getCrt());
-                KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
-                keyStore.load(null, null);
-
-                keyStore.setCertificateEntry(center.getName(), certificate);
-                TrustManagerFactory trustManagerFactory = TrustManagerFactory
-                                .getInstance(TrustManagerFactory.getDefaultAlgorithm());
-                trustManagerFactory.init(keyStore);
-                SslContext sslContext = sslBuilder.trustManager(trustManagerFactory).build();
-                HttpClient httpClient = HttpClient.create()
-                                .secure(sslSpec -> sslSpec.sslContext(sslContext));
-                return WebClient.builder().clientConnector(new ReactorClientHttpConnector(httpClient))
-                                .baseUrl("https://" + center.getIp() + ":" + center.getPort()).build();
-        }
-
-        public WebClient agent2AgentWebClient(int agent_id) throws Exception {
+        public WebClient center2AgentWebClient(int agent_id) throws Exception {
                 LambdaQueryWrapper<Agent> queryWrapper = Wrappers.<Agent>lambdaQuery().eq(Agent::getUid, agent_id);
                 Agent agent = agentMapper.selectOne(queryWrapper);
                 Certificate certificate = Utlis.bytesToCertificate(agent.getCrt());
