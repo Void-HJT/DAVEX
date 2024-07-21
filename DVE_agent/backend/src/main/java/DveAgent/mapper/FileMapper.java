72a2ad17e36c4882de6afccee8c0df4dd9306abf
tongji-TTT <76111264+tongji-TTT@users.noreply.github.com>
2024-07-21 10:32:32 +0800

    factor："文件夹与文件管理"

--- DVE_agent/backend/src/main/java/DveAgent/mapper/FileMapper.java
+++ DVE_agent/backend/src/main/java/DveAgent/mapper/FileMapper.java
@@ -1,10 +1,14 @@
 package DveAgent.mapper;
 
 
+import DveAgent.common.Body;
 import DveAgent.entity.File;
+import DveAgent.info.FileInfo;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface FileMapper extends BaseMapper<File> {
+    FileInfo getFileInfo(@Param("fileId") Integer uid, @Param("agentId") Integer agentId, @Param("folderId") Integer folderId);
 }
