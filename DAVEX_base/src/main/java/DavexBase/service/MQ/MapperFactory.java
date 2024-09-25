0d1b9a1ab5666af9c6cdb4131014712fa4c8e39d
tongji-TTT <76111264+tongji-TTT@users.noreply.github.com>
2024-09-25 16:44:05 +0800

    feat: Folder与File 同步实现2.0

---
+++ DAVEX_base/src/main/java/DavexBase/service/MQ/MapperFactory.java
@@ -0,0 +1,51 @@
+package DavexBase.service.MQ;
+
+import DavexBase.entity.*;
+import DavexBase.mapper.*;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class MapperFactory {
+
+    // 定义一个 Map，存储 Mapper 和它对应的实体类
+    private final Map<String, BaseMapper<?>> mapperMap = new HashMap<>();
+    private final Map<String, Class<?>> entityTypeMap = new HashMap<>();
+
+    @Autowired
+    public MapperFactory(ApplicationMapper applicationMapper, CenterMapper centerMapper, AgentMapper agentMapper,
+                         FileMapper fileMapper, FolderMapper folderMapper) {
+        // 注册不同的 Mapper 实现类
+        mapperMap.put("application", applicationMapper);
+        entityTypeMap.put("application", Application.class);
+
+        mapperMap.put("center", centerMapper);
+        entityTypeMap.put("center", Center.class);
+
+        mapperMap.put("agent", agentMapper);
+        entityTypeMap.put("agent", Agent.class);
+
+        mapperMap.put("file", fileMapper);
+        entityTypeMap.put("file", File.class);
+
+        mapperMap.put("folder", folderMapper);
+        entityTypeMap.put("folder", Folder.class);
+    }
+
+    // 根据类名获取相应的 Mapper 实例
+    public BaseMapper<?> getMapper(String className) {
+        return mapperMap.get(className.toLowerCase());
+    }
+
+    // 根据类名获取对应的实体类
+    public Class<?> getEntityType(String className) {
+        return entityTypeMap.get(className.toLowerCase());
+    }
+}
+
+
+
