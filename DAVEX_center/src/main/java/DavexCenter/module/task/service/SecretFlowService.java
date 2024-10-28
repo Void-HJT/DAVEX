67d06f487a0d93e4576d72e45b09f9e6eb6318fb
merlinhuahua <2418529928@qq.com>
2024-10-28 14:33:45 +0800

    feat: fdLresult

--- DAVEX_center/src/main/java/DavexCenter/module/task/service/SecretFlowService.java
+++ DAVEX_center/src/main/java/DavexCenter/module/task/service/SecretFlowService.java
@@ -1,14 +1,30 @@
 package DavexCenter.module.task.service;
 
+import DavexCenter.mapper.OutputMapper;
+import DavexCenter.module.file.service.FlFileService;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+import DavexCenter.module.file.controller.FlFileController;
+import DavexCenter.module.file.service.FileService;
+import DavexBase.common.My;
+import DavexBase.common.Body;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.beans.factory.annotation.Autowired;
+
 
 @Service
 public class SecretFlowService {
+    @Autowired
+    private FileService fileService;
+    @Autowired
+    private FlFileController flFileController;
+    @Autowired
+    private My my;
     // 方法接受一个字符串参数作为命令，并执行它
     public String executeCommand(String command) {
         try {
@@ -45,6 +61,10 @@ public class SecretFlowService {
             e.printStackTrace();
             return "Error occurred: " + e.getMessage();
         }
+    }
+    public Body<String> saveFile (MultipartFile file){
+        String hash = fileService.getSha256(file);
+        return flFileController.saveFl(file,hash,my.getId(),null);
     }
 }
 
