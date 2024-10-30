b46572f97eb9702c5471c3ed2db7c4a08b288bc1
Kevin <276389081@qq.com>
2024-10-30 10:58:02 +0800

    Refactor: 前端MPC跑通

--- DAVEX_ui/src/api/pSi.js
+++ DAVEX_ui/src/api/pSi.js
@@ -1,13 +1,31 @@
 import request from '@/utils/request'
 
-export const createPsiTask = ({ formData }) => {
-  return request({
-    url: '/MpcTasks/create_with_input',
-    method: 'post',
-    data: formData,
-    headers: {
-      'Content-Type': 'multipart/form-data',
-      Accept: '*/*'
-    },
-  });
-};
+// export const createPsiTask = ({ formData }) => {
+//   return request({
+//     url: '/MpcTasks/create_with_input',
+//     method: 'post',
+//     data: formData,
+//     headers: {
+//       'Content-Type': 'multipart/form-data',
+//       Accept: '*/*'
+//     },
+//   });
+// };
+
+export const createPsiTask = ({ file, mpcTask }) => {
+  const formData = new FormData()
+  formData.append('file', file)
+  const mpcTaskJson = JSON.stringify(mpcTask)
+  const mpcTaskBlob = new Blob([mpcTaskJson], { type: 'application/json' })
+  formData.append('mpcTask', mpcTaskBlob) // 添加 mpcTask，指定类型
+  let res = request.post(
+      '/MpcTasks/create_with_input',
+      formData,
+      {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        }
+      }
+  )
+  return res
+}
